ed54fd3d93c240ff431af55d065a82509892af38
Kraily4t8 <53635947+Kraily4t8@users.noreply.github.com>
2024-01-27 15:59:06 -0800

    state machine test initial

---
+++ testEnemyStateMachine.js
@@ -0,0 +1,15 @@
+class Enemy {
+    constructor(game, x, y) {
+        Object.assign(this, { game, x, y});
+        this.currentState = "idle";
+        
+    }
+
+    update() {
+
+    }
+
+    draw() {
+
+    }
+}
