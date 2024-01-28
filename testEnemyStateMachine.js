dbee5260487e889e5428cd7c11450d76053e24db
Kraily4t8 <53635947+Kraily4t8@users.noreply.github.com>
2024-01-27 16:03:50 -0800

    figuring out state template

--- testEnemyStateMachine.js
+++ testEnemyStateMachine.js
@@ -1,15 +1,36 @@
 class Enemy {
     constructor(game, x, y) {
         Object.assign(this, { game, x, y});
-        this.currentState = "idle";
         
+        this.States = {
+            Idle: "idle",
+            Walking: "walking",
+            Attack: "attack",
+            Jumping: "jumping",
+            Falling: "falling",
+            Damage: "damage"
+        }
+
+        this.currentState = this.States.Idle;
     }
 
     update() {
-
+        switch(this.currentState) {
+            case this.States.Idle:
+                this.Idle();
+                break;
+        }
     }
 
-    draw() {
+    draw(ctx) {
+        switch(this.currentState) {
+            case this.States.Idle:
+                
+                break;
+        }
+    }
+
+    Idle() {
 
     }
 }
