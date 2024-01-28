58d37951f13692bc5874377235e22d9ceda1df14
Kraily4t8 <53635947+Kraily4t8@users.noreply.github.com>
2024-01-27 20:37:16 -0800

    attempt at miku statemanager

--- simpleEnemyState.js
+++ simpleEnemyState.js
@@ -1,25 +1,21 @@
 //Author: Thomas Tien
-//use this as a simple example for state machines
+//use this as a simple example for complex state machines
 
 class Enemy {
     constructor(game, x, y) {
         Object.assign(this, { game, x, y});
-
-        this.currentState = new EnemyIdle(this.game, this.x, this.y);
-        this.stateName = "idle";
+        this.currentState = new EnemyIdle(this);
+        this.stateName = "idle";//states: idle, walk
     }
     
     update() {
-        console.log("Enemy " + this.x + " " + this.y);
+        // console.log("Enemy " + this.x + " " + this.y);
         let newState = this.currentState.update();
-        
-        //update position so other classses can see
-        this.x = this.currentState.x;
-        this.y = this.currentState.y;
 
+        //if it's a new state, switch to that state
         if(newState != this.stateName) {
-            // console.log(newState.name);
             this.stateName = newState.name;
+            delete this.currentState;
             this.currentState = newState;
         }
     }
@@ -30,9 +26,9 @@ class Enemy {
 }
 
 class EnemyIdle {
-    constructor(game, x, y) {
+    constructor(stateManager) {
         //on enter
-        Object.assign(this, { game, x, y});
+        Object.assign(this, {stateManager});
         this.name = "idle";
 
         this.idleTime = 0;
@@ -47,20 +43,20 @@ class EnemyIdle {
         //condition to exit
         if(this.idleTime >= this.idleDuration) {
             // return "walk";
-            return new EnemyWalk(this.game, this.x, this.y);
+            return new EnemyWalk(this.stateManager);
         }
         return output;
     }
 
     draw(ctx) {
         ctx.fillStyle = "Green";
-        ctx.fillRect(this.x, this.y, 42, 86);
+        ctx.fillRect(this.stateManager.x, this.stateManager.y, 42, 86);
     }
 }
 
 class EnemyWalk {
-    constructor(game, x, y) {
-        Object.assign(this, { game, x, y});
+    constructor(stateManager) {
+        Object.assign(this, {stateManager});
         this.name = "walk";
 
         this.wanderTime = 0;
@@ -79,17 +75,17 @@ class EnemyWalk {
             this.direction *=-1;
             this.wanderTime = 0;
         }
-        this.x +=this.direction /2;
+        this.stateManager.x +=this.direction /2;
 
         //condition to exit
         if(this.walkTime >= this.walkDuration) {
-            return new EnemyIdle(this.game, this.x, this.y);
+            return new EnemyIdle(this.stateManager);
         }
         return output;
     }
 
     draw(ctx) {
         ctx.strokeStyle = "Red";
-        ctx.strokeRect(this.x, this.y, 42, 86);
+        ctx.strokeRect(this.stateManager.x, this.stateManager.y, 42, 86);
     }
 }
