57b35c5c788f7ffa878a33b4f47f21ae7879942c
Doplr <49573929+DaddyDoplr@users.noreply.github.com>
2024-01-31 22:41:18 -0800

    Added tile Objects with textures
    GrassTile and StoneTile objects in the blocks.js class, being created in the scenemanager class at lines 32 and 33

--- blocks.js
+++ blocks.js
@@ -16,3 +16,45 @@ class Ground {
         ctx.strokeRect(this.x, this.y, this.w, PARAMS.BLOCKWIDTH);
     };
 }
+
+class GrassTile {
+    constructor(game, x, y, w) {
+        Object.assign(this, { game, x, y, w});
+        this.spritesheet = ASSET_MANAGER.getAsset("./assets/bg_groundTiles.png");
+
+        this.BB = new BoundingBox(this.x, this.y, this.w, PARAMS.BLOCKWIDTH, "ground");
+        // this.leftBB = new BoundingBox(this.x, this.y, PARAMS.BLOCKWIDTH, PARAMS.BLOCKWIDTH * 2)
+        // this.rightBB = new BoundingBox(this.x + this.w - PARAMS.BLOCKWIDTH, this.y, PARAMS.BLOCKWIDTH, PARAMS.BLOCKWIDTH * 2)
+    };
+
+    update() {
+
+    };
+
+    draw(ctx, game) {
+        ctx.drawImage(this.spritesheet, 0, 0, 32, 32, this.x, this.y, this.w, this.w);
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(this.x, this.y, this.w, this.w);
+    };
+}
+
+class StoneTile {
+    constructor(game, x, y, w) {
+        Object.assign(this, { game, x, y, w});
+        this.spritesheet = ASSET_MANAGER.getAsset("./assets/bg_groundTiles.png");
+
+        this.BB = new BoundingBox(this.x, this.y, this.w, PARAMS.BLOCKWIDTH, "ground");
+        // this.leftBB = new BoundingBox(this.x, this.y, PARAMS.BLOCKWIDTH, PARAMS.BLOCKWIDTH * 2)
+        // this.rightBB = new BoundingBox(this.x + this.w - PARAMS.BLOCKWIDTH, this.y, PARAMS.BLOCKWIDTH, PARAMS.BLOCKWIDTH * 2)
+    };
+
+    update() {
+
+    };
+
+    draw(ctx, game) {
+        ctx.drawImage(this.spritesheet, 32, 0, 32, 32, this.x, this.y, this.w, this.w);
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(this.x, this.y, this.w, this.w);
+    };
+}
