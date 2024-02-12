d1fe7f7eb1d3b4e52022e8c9c0ab4dd7728db332
Doplr <49573929+DaddyDoplr@users.noreply.github.com>
2024-02-12 15:10:42 -0800

    small updates to level class and tile texture

--- levels.js
+++ levels.js
@@ -1,67 +1,106 @@
-class levelOne {
+class levelFunctions {
     constructor() {
-        let slime = new Slime(200, 480);
 
+    }
 
-        this.gameEngine.addEntity(slime);
-        this.gameEngine.addEntity(new SpecterKnight(this.gameEngine, 300, 100, ASSET_MANAGER.getAsset("./assets/specter knight.png")));
-        //this.gameEngine.addEntity(new Ground(this.gameEngine, 100, 300, 50));
-        //this.gameEngine.addEntity(new Ground(this.gameEngine, 30, 600, 800));
-        // this.gameEngine.addEntity(new Miku(this.gameEngine, 50, 50, ASSET_MANAGER.getAsset("./assets/miku spritesheet.png")));
-        this.gameEngine.addEntity(new Player(this.gameEngine, 0, 300, ASSET_MANAGER.getAsset("./assets/pack_loreon_char_free_modified.png")));
-        // Creating textured environment tiles (X and Y are multiplied by the size defined in each block's class)
-        this.gameEngine.addEntity(new GrassTile(this.gameEngine, 15, 2));
-        this.gameEngine.addEntity(new DirtTile(this.gameEngine, 15, 3));
-        this.gameEngine.addEntity(new StoneTile(this.gameEngine, 16, 2));
-        this.gameEngine.addEntity(new DevTile(this.gameEngine, 16, 3));
-        this.gameEngine.addEntity(new GrassTile(this.gameEngine, 12, 9));
+    buildFloor(theType, theStartX, theLength, theLevel, theArray) {
+        
+        // if(PARAMS.DEBUG) console.log(theType);
 
+        if (theType == '0') {
+            for (var i = 0; i < theLength; i++) {
+                theArray.push(new GrassTile(this.gameEngine, theStartX + i, theLevel));
+            };
+        } else if (theType == '1') {
+            for (var i = 0; i < theLength; i++) {
+                theArray.push(new StoneTile(this.gameEngine, theStartX + i, theLevel));
+            }
+        } else if (theType == '2') {
+            for (var i = 0; i < theLength; i++) {
+                theArray.push(new DirtTile(this.gameEngine, theStartX + i, theLevel));
+            }
+        } else {
+            for (var i = 0; i < theLength; i++) {
+                theArray.push(new DevTile(this.gameEngine, theStartX + i, theLevel));
+            }
+        };
+    };
+}
 
+class levelOne {
+    constructor(theGame) {
+        this.functions = new levelFunctions;
+        this.assets = [];
+        this.build(theGame);
+    }
+
+    build(theGame) {
+
+        this.assets.push(new skelly(theGame, 400, 420, ASSET_MANAGER.getAsset("./assets/Skeleton_spritesheet.png")));
+        this.assets.push(new SpecterKnight(theGame, 300, 100, ASSET_MANAGER.getAsset("./assets/specter knight.png")));
+        this.assets.push(new Player(theGame, 0, 300, ASSET_MANAGER.getAsset("./assets/pack_loreon_char_free_modified.png")));
 
         // Types: 0 - Grass | 1 - Stone | 2 - Dirt | Any Other Int - Dev
-        // Second argument is the total length in blocks
-        this.drawFloor(0, 0, 13, 11);
-        this.drawFloor(2, 0, 13, 12);
-        this.drawFloor(2, 0, 13, 13);
+        // Second argument is the X start position
+        // Third argument is the total length in blocks
+        // Fourth argument is the Y level
+        // Fifth argument is the array to store assets
+        this.functions.buildFloor(0, 0, 13, 11, this.assets);
+        this.functions.buildFloor(2, 0, 13, 12, this.assets);
+        this.functions.buildFloor(2, 0, 13, 13, this.assets);
 
-        this.drawFloor(0, 16, 7, 11);
-        this.drawFloor(2, 16, 7, 12);
-        this.drawFloor(2, 16, 7, 13);
+        this.functions.buildFloor(0, 16, 7, 11, this.assets);
+        this.functions.buildFloor(2, 16, 7, 12, this.assets);
+        this.functions.buildFloor(2, 16, 7, 13, this.assets);
 
         // items
-        this.gameEngine.addEntity(new Chest(this.gameEngine, 9, 10));
-
+        this.assets.push(new Chest(theGame, 9, 10));
 
         // Draw Background last
-        this.gameEngine.addEntity(new Background(this.gameEngine, 0));
+        this.assets.push(new Background(theGame, 0));
     }
+
+    getAssets() {
+        return this.assets;
+    }
+
 }
 
 // not quite different enough from level one, it's currently a placeholder
 class levelTwo {
-    constructor() {
-        let slime = new Slime(200, 480);
+    constructor(theGame) {
+        this.functions = new levelFunctions;
+        this.assets = [];
+        this.build(theGame);
+    }
 
-        this.gameEngine.addEntity(slime);
-        this.gameEngine.addEntity(new SpecterKnight(this.gameEngine, 300, 100, ASSET_MANAGER.getAsset("./assets/specter knight.png")));
-        this.gameEngine.addEntity(new Player(this.gameEngine, 0, 300, ASSET_MANAGER.getAsset("./assets/pack_loreon_char_free_modified.png")));
-        this.gameEngine.addEntity(new StoneTile(this.gameEngine, 12, 9));
-        
+    build(theGame) {
+
+        this.assets.push(new skelly(theGame, 400, 420, ASSET_MANAGER.getAsset("./assets/Skeleton_spritesheet.png")));
+        this.assets.push(new SpecterKnight(theGame, 300, 100, ASSET_MANAGER.getAsset("./assets/specter knight.png")));
+        this.assets.push(new Player(theGame, 0, 300, ASSET_MANAGER.getAsset("./assets/pack_loreon_char_free_modified.png")));
+
         // Types: 0 - Grass | 1 - Stone | 2 - Dirt | Any Other Int - Dev
-        // Second argument is the total length in blocks
-        this.drawFloor(1, 0, 13, 11);
-        this.drawFloor(1, 0, 13, 12);
-        this.drawFloor(1, 0, 13, 13);
+        // Second argument is the X start position
+        // Third argument is the total length in blocks
+        // Fourth argument is the Y level
+        // Fifth argument is the array to store assets
+        this.functions.buildFloor(1, 0, 13, 11, this.assets);
+        this.functions.buildFloor(1, 0, 13, 12, this.assets);
+        this.functions.buildFloor(1, 0, 13, 13, this.assets);
 
-        this.drawFloor(1, 16, 7, 11);
-        this.drawFloor(1, 16, 7, 12);
-        this.drawFloor(1, 16, 7, 13);
+        this.functions.buildFloor(1, 16, 7, 11, this.assets);
+        this.functions.buildFloor(1, 16, 7, 12, this.assets);
+        this.functions.buildFloor(1, 16, 7, 13, this.assets);
 
         // items
-        this.gameEngine.addEntity(new Chest(this.gameEngine, 9, 10));
-
-
+        this.assets.push(new Chest(theGame, 9, 10));
+        
         // Draw Background last
-        this.gameEngine.addEntity(new Background(this.gameEngine, 0));
+        this.assets.push(new Background(theGame, 0));
+    }
+
+    getAssets() {
+        return this.assets;
     }
 }
