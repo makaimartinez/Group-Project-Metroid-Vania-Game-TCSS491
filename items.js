5f2abf27d159c51cf4f92650b8828044e6209790
makai <makaimartinez.edu@gmail.com>
2024-02-12 17:46:18 -0800

    Update items.js
    changing some magic numbers to already existing variables

--- items.js
+++ items.js
@@ -31,9 +31,10 @@ class SpeedPotion {
         this.x *= PARAMS.BLOCKWIDTH;
         this.y *= PARAMS.BLOCKWIDTH;
         this.spritesheet = ASSET_MANAGER.getAsset("./assets/speedpotion.png");
-        this.size = 48; // 48 x 48 px sprite but potion is placed in center different
+        this.size = 48; // 48 x 48 px sprite but potion is smaller and placed in center so...
+        this.BBsize = 32; // 32 x 32 pixels is a decently sized hit box around the actual size
         this.removeFromWorld = false;
-        this.BB = new BoundingBox(this.x + 8, this.y  + 20, 32, 32, "speedpotion");
+        this.BB = new BoundingBox(this.x + 8, this.y  + 20, this.BBsize, this.BBsize, "speedpotion");
        
     };
 
@@ -52,8 +53,9 @@ class SpeedPotion {
 
     draw(ctx) {
         ctx.drawImage(this.spritesheet, 0, 0, this.size, this.size, this.x, this.y, PARAMS.BLOCKWIDTH, PARAMS.BLOCKWIDTH);
+
         ctx.strokeStyle = "red";
-        ctx.strokeRect(this.x + 8, this.y + 20, 32, 32);
+        ctx.strokeRect(this.BB.x, this.BB.y, this.BBsize, this.BBsize);
     };
 }
 
@@ -65,9 +67,10 @@ class HealthPotion {
         this.x *= PARAMS.BLOCKWIDTH;
         this.y *= PARAMS.BLOCKWIDTH;
         this.spritesheet = ASSET_MANAGER.getAsset("./assets/healthpotion.png");
-        this.size = 48; // 48 x 48 px sprite but potion is placed in center different
+        this.size = 48; 
+        this.BBsize = 32;
         this.removeFromWorld = false;
-        this.BB = new BoundingBox(this.x + 8, this.y  + 20, 32, 32, "healthpotion");
+        this.BB = new BoundingBox(this.x + 8, this.y  + 20, this.BBsize, this.BBsize, "healthpotion");
        
     };
 
@@ -85,7 +88,7 @@ class HealthPotion {
     draw(ctx) {
         ctx.drawImage(this.spritesheet, 0, 0, this.size, this.size, this.x, this.y, PARAMS.BLOCKWIDTH, PARAMS.BLOCKWIDTH);
         ctx.strokeStyle = "red";
-        ctx.strokeRect(this.x + 8, this.y + 20, 32, 32);
+        ctx.strokeRect(this.BB.x, this.BB.y, this.BBsize, this.BBsize);
     };
 }
 
